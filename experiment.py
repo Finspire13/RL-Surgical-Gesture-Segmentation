f86b87d415a36fedade80218865f5a344fa872f6
Unknown <finspire13@gmail.com>
2018-01-30 20:25:24 +0800

    Uncommented

--- experiment.py
+++ experiment.py
@@ -40,8 +40,7 @@ def experiment_trpo(naming):
             for run_idx in range(1, 1 + trpo_train_run_num):
 
                 processes = []
-                #for feature_type in ['sensor', 'visual']:
-                for feature_type in ['sensor']:
+                for feature_type in ['sensor', 'visual']:
                     formatted_args = cmd_args.format(
                         feature_type, tcn_run_idx, split_idx, run_idx)
                     full_cmd = trpo_train_cmd + formatted_args
@@ -53,30 +52,30 @@ def experiment_trpo(naming):
                     raise Exception('Subprocess Error!')
 
 
-    # # Get Averaged Results: TRPO
-    # template = 'result_{}_tcn_{}_split_{}_run_{}.npy'
-    # for feature_type in ['sensor', 'visual']:
-    #     trpo_result = np.zeros((tcn_run_num, split_num,
-    #                         trpo_train_run_num, trpo_test_run_num, 9))
-    #     for tcn_run_idx in range(1, 1 + tcn_run_num):
-    #         for split_idx in range(1, 1 + split_num):
-    #             for run_idx in range(1, 1 + trpo_train_run_num):
-    #                 run_result_file = template.format(
-    #                     feature_type, tcn_run_idx, split_idx, run_idx)
-    #                 run_result_file = os.path.join(result_dir, run_result_file)
-    #                 run_result = np.load(run_result_file)
-    #                 run_result = run_result.mean(0)
-    #                 trpo_result[tcn_run_idx-1,split_idx-1,run_idx-1,:,:] = run_result
-    #                 os.remove(run_result_file)
+    # Get Averaged Results: TRPO
+    template = 'result_{}_tcn_{}_split_{}_run_{}.npy'
+    for feature_type in ['sensor', 'visual']:
+        trpo_result = np.zeros((tcn_run_num, split_num,
+                            trpo_train_run_num, trpo_test_run_num, 9))
+        for tcn_run_idx in range(1, 1 + tcn_run_num):
+            for split_idx in range(1, 1 + split_num):
+                for run_idx in range(1, 1 + trpo_train_run_num):
+                    run_result_file = template.format(
+                        feature_type, tcn_run_idx, split_idx, run_idx)
+                    run_result_file = os.path.join(result_dir, run_result_file)
+                    run_result = np.load(run_result_file)
+                    run_result = run_result.mean(0)
+                    trpo_result[tcn_run_idx-1,split_idx-1,run_idx-1,:,:] = run_result
+                    os.remove(run_result_file)
 
-    #     trpo_result_file = 'trpo_avg_result_{}_{}.npy'.format(feature_type, naming)
-    #     trpo_result_file = os.path.join(result_dir, trpo_result_file)
-    #     #np.save(trpo_result_file, trpo_result.mean(0).mean(0).mean(0).mean(0))
-    #     np.save(trpo_result_file, trpo_result)
+        trpo_result_file = 'trpo_avg_result_{}_{}.npy'.format(feature_type, naming)
+        trpo_result_file = os.path.join(result_dir, trpo_result_file)
+        #np.save(trpo_result_file, trpo_result.mean(0).mean(0).mean(0).mean(0))
+        np.save(trpo_result_file, trpo_result)
 
 
 def main():
-    #experiment_tcn()
+    experiment_tcn()
     experiment_trpo('baseline')
 
 
